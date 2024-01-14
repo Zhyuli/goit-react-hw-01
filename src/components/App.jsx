e68c4be13030cd7602932fa4a7e4d5be2a9b777f
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2024-01-14 22:51:48 +0200

    folders with .module.css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import userData from "../userData.json"
 import friends from "../friends.json"
-import { Profile } from "./Profile"
-import { FriendList } from "./FriendList";
+import { Profile } from "./Profile/Profile"
+import { FriendList } from "./FriendList/FriendList";
 import transactions from "../transactions.json"
-import { TransactionHistory } from "./TransactionHistory";
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
 
 
 
