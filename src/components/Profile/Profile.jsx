4972421c16fd700d696bdd6b75392d27cb51fb35
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2024-01-15 20:16:04 +0200

    Profile css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,29 +2,29 @@
 import css from './Profile.module.css';
 
 export const Profile = ({ name, tag, location, stats, image }) => {
-    return (<div className="{css.profile}">
+    return (<div className={css.profile}>
   <div className={css.description}>
     <img
       src={image}
       alt="User avatar"
       className={css.avatar}
     />
-            <p className="{css.name}">{name}</p>
+            <p className={css.name}>{name}</p>
     <p className={css.tag}>@{tag}</p>
     <p className={css.location}>{location}</p>
   </div>
 
 
   <ul className={css.stats}>
-    <li>
+    <li className={css['stats-item']}>
       <span className="label">Followers</span>
       <span className={css.value}>{stats.followers}</span>
     </li>
-    <li>
+    <li className={css['stats-item']}>
       <span className="label">Views</span>
       <span className={css.value}>{stats.views}</span>
     </li>
-    <li>
+    <li className={css['stats-item']}>
       <span className="label">Likes</span>
       <span className={css.value}>{stats.likes}</span>
     </li>
